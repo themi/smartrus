7982ee2c188a1248b00d403ca1364a178e478a07
Tim Hemi (Ops) <tim@reinteractive.net>
2018-06-04 08:20:37 +1000

    show course decendant lessons

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,24 @@
 module CoursesHelper
+
+  def ancestor_breadcrumbs_for(item)
+    content_tag(:nav, "aria-label": "breadcrumb") do
+      concat(
+        content_tag(:ol, class: "breadcrumb") do
+          item.ancestors.each do |parent|
+            concat content_tag(:li, link_to(parent.name, parent), class: "breadcrumb-item")
+          end
+        end
+      )
+    end
+  end
+
+  def descendant_list_for(parent_item)
+    content_tag :ul do
+      parent_item.children.each do |child|
+        concat content_tag(:li, link_to(child.name, child))
+        concat descendant_list_for(child)
+      end
+    end
+  end
+
 end
