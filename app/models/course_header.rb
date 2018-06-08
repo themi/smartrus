a677b60af6aa4a5ef75f7380208a996fd9d60822
Tim Hemi (Ops) <tim@reinteractive.net>
2018-06-08 23:03:24 +1000

    annotate models

--- app/models/course_header.rb
+++ app/models/course_header.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: course_headers
+#
+#  id         :bigint(8)        not null, primary key
+#  course_id  :bigint(8)
+#  status     :integer          default("editing")
+#  owner_id   :integer
+#  owner_type :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class CourseHeader < ApplicationRecord
   enum status: [:editing, :review, :published, :restricted, :archived]
   belongs_to :course
